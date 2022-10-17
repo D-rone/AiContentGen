251b573086fca3ee715211632d256e2a508af669
Drone <medouksili@gmail.com>
2022-10-17 16:08:01 +0100

    getting response from aicontent to app

--- aicontent.py
+++ aicontent.py
@@ -5,16 +5,24 @@ import config
 openai.api_key = config.OPENAI_API_KEY
 
 
-def productDeescription(query)
-    reponse = openai.Completion.create(
+def productDescription(query):
+    response = openai.Completion.create(
         engin = "davinci-instruct-beta-v3",
-        prompt = "",
+        prompt = " {}".format(query), ################################## NEED TO ADD THIS KEY FROM YOUR OPENAI ACCOUNT before {} !!!!!!!!!!!!
         temperature = 0.5,
         max_tokes=200,
         top_p =1,
         frequency_penalty=0,
         presence_penalty=0
     )
-    print (reponse)
-query = "apple iphone"
-productDeescription(query)
+    if "choices" in response:
+        if len(response["choices"]) > 0 :
+            answer = response["choices"][0]["text"]
+        else:
+            ansewr = "Oppps sorry, you beat the AI this time"
+    else:
+        ansewr = "Oppps sorry, you beat the AI this time"
+
+    return(answer)
+
+
